a062ebb086e339d71b0e3a006fa3414bac216f76
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 12:04:49 +0200

    Maj-views.py

--- users/permissions.py
+++ users/permissions.py
@@ -1,25 +1,26 @@
 from rest_framework.permissions import BasePermission
 
-class CanCreateUser(BasePermission):
+# Differentes actions du crud
+class CanCreateUser(BasePermission): #create
     def has_permission(self, request, view):
         return request.user.has_perm("users.add_user")
 
-class CanListUsers(BasePermission):
+class CanListUsers(BasePermission): #read liste
     def has_permission(self, request, view):
         return request.user.has_perm("users.view_user")
 
-class CanRetrieveUser(BasePermission):
+class CanRetrieveUser(BasePermission): #read detail
     def has_permission(self, request, view):
         return request.user.has_perm("users.view_user")
 
-class CanUpdateUser(BasePermission):
+class CanUpdateUser(BasePermission):  #update complet
     def has_permission(self, request, view):
         return request.user.has_perm("users.update_user")
 
-class CanPartialUpdateUser(BasePermission):
+class CanPartialUpdateUser(BasePermission):  #update partiel
     def has_permission(self, request, view):
         return request.user.has_perm("users.update_user")
 
-class CanDeleteUser(BasePermission):
+class CanDeleteUser(BasePermission):  #delete
     def has_permission(self, request, view):
         return request.user.has_perm("users.delete_user")
--- users/views.py
+++ users/views.py
@@ -1,11 +1,62 @@
 # from django.contrib.auth.models import User
-from rest_framework import permissions, viewsets
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Group
+from django.contrib.auth.password_validation import validate_password
+from django.shortcuts import get_object_or_404
+
+from rest_framework import views, viewsets
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import Token
+
 from .models import User
+from .permissions import (
+    CanCreateUser,
+    CanDeleteUser,
+    CanListUsers,
+    CanPartialUpdateUser,
+    CanRetrieveUser,
+    CanUpdateUser,
+)
+from .serializers import UserCreationSerializer, UserLoginSerializer, UserSerializer
 
-from .serializers import UserSerializer
+class UserLoginView(views.APIView):
+    permission_classes = []
+    serializer_class = UserLoginSerializer
 
+    def post(self: views, request: views.Request) -> Response: 
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by("-date_joined")
+        if serializer.is_valid() or True: 
+            user = authenticate(
+                username = serializer.validated_data["username"],
+                password = serializer.validated_data["password"],
+            )
+
+            if user: 
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({"token": token.key})
+            else: 
+                return Response({"error": 'Invalid credentials'}, status=401)
+        return Response(serializer.errors, status=400)
+
+class UserViewSet(viewsets.ViewSet): 
+    permission_classes = []
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [CanCreateUser]
+        elif self.action == 'list':
+            self.permission_classes = [CanListUsers]
+        elif self.action == 'retrieve':
+            self.permission_classes = [CanRetrieveUser]
+        elif self.action == 'update':
+                    self.permission_classes = [CanUpdateUser]
+        elif self.action == 'partial_update':
+                    self.permission_classes = [CanPartialUpdateUser]
+        elif self.action == 'destroy':
+                    self.permission_classes = [CanDeleteUser]
+
+        return [permission() for permission in self.permission_classes]
+    
