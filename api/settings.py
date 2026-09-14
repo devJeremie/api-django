aafa409e997d5cd9c763f05692ac82ef1b76af14
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 14:54:57 +0200

    Maj-Settings.py

--- api/settings.py
+++ api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'users',
 ]
 
@@ -144,4 +145,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS":
     "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE":10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
 }
--- api/urls.py
+++ api/urls.py
@@ -3,15 +3,11 @@ Configuration URL du projet
 """
 
 from django.urls import include, path
-from rest_framework import routers
-from users import views
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path("api-auth/", include("rest_framework.urls",
-        namespace="rest_framework")),
+    path('', include("users.urls")),
+    path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refreshtoken", TokenRefreshView.as_view(), name="token_refresh"),
 ]
