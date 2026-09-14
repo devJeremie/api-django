b998b74745253f2edc6e1e9ef1cde3763f9ca393
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 11:13:57 +0200

    MAJ-ServerValide-models-user-bdd

--- api/settings.py
+++ api/settings.py
@@ -74,11 +74,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {  modele de base
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api-rest-django',#db name
+        'USER' : 'root', #db-user
+        'PASSWORD' : '', #db userpassword
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }    
 }
 
 
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
+from .models import User
 
 from .serializers import UserSerializer
 
