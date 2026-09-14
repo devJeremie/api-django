6f5a2f5802320d96cbf211df5ef37ef88175b5bb
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 14:29:13 +0200

    Maj-End-views.py

--- users/views.py
+++ users/views.py
@@ -59,4 +59,79 @@ class UserViewSet(viewsets.ViewSet):
                     self.permission_classes = [CanDeleteUser]
 
         return [permission() for permission in self.permission_classes]
+
+    def create(self, request):
+        user = User(**request.data)
+        validate_password(request.data.get("password", ""),user=user)
+        user.set_password(request.data.get("password", ""))
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            if user.role == User.ADMIN:
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+
+                group = Group.objects.get(name="admin")
+                group.user_set.add(user)
+
+            elif user.role == User.MODERATOR:
+                user.is_staff = True
+                user.is_superuser = False
+                user.save()
+
+                group = Group.objects.get(name="moderator")
+                group.user_set.add(user)
+
+            else: #User role is USER
+                user.is_staff = False
+                user.is_superuser = False
+                user.save()
+
+                group = Group.objects.get(name="user")
+                group.user_set.add(user)
+
+            return Response(serializer.data, status=201)
+        
+        return Response(serializer.errors, status=400)
+
+    def list(self, _):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
+    
+    def retrieve(self, _, pk=None):
+        user = get_object_or_404(User, id=pk)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        user = get_object_or_404(User, id=pk)
+        serializer = UserSerializer(user, data=request.data, partial=False)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(UserSerializer(user).data)
+        
+        return Response(serializer.errors, status=400)
+
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(User, id=pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            return Response(UserSerializer(user).data)
+
+        return Response(serializer.errors, status=400)
+
+    def destroy(self, _, pk=None):
+        user = get_object_or_404(User, id=pk)
+        user.delete()
+
+        return Response(status=204)
     
