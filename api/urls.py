90fcfca27048fcc731d6dd49e198e1390118be92
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 15:52:46 +0200

    Maj-urls.py

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', include("users.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refreshtoken", TokenRefreshView.as_view(), name="token_refresh"),
 ]
