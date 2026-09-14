b2d47377c79ccb715aa85f41237b1b815ff56fd3
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 10:07:35 +0200

    MAJ- Serializer-User

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 # from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from models import User
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer): 
     class Meta : 
@@ -19,3 +19,32 @@ class UserSerializer(serializers.ModelSerializer):
             "date_joined",
             "last_login",
         ]
+class UserCreationSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = [
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+        ]
+
+    extra_kwargs = {
+        "username":{"required": True},
+        "password":{"required": True},
+        "email":{"required": True},
+        "role":{"default": User.USER},
+    }
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    model = User
+    fields = [
+        "username",
+        "password",
+    ]
+    extra_kwargs = {
+        "username":{"required": True},
+        "password":{"required": True},
+    }
