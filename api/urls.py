931d3d9d97c38982a6c7bb745e8d5f34fb99a733
devJeremie <chabanaisjeremie@gmail.com>
2026-09-14 16:04:18 +0200

    Maj-urlAPi,urlUsers

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ Configuration URL du projet
 """
 
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
